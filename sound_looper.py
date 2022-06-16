b54201d4ca503454b339e3b9b7c69e4b0e4faf3a
Halfdan Hauch Jensen <halfdanhauchjensen@gmail.com>
2022-06-16 12:57:54 +0200

    Create sound_looper.py

---
+++ sound_looper.py
@@ -0,0 +1,14 @@
+import pygame
+import time
+
+time.sleep(10) # wait for the UDB drive to mount
+
+pygame.mixer.init()
+#pygame.mixer.music.load("sound.wav")
+pygame.mixer.music.load("/media/air/AIR_DISK1/sound.wav")
+pygame.mixer.music.play(-1) # note -1 for playing in loops
+while pygame.mixer.music.get_busy(): 
+    pygame.time.Clock().tick(10)
+    
+
+# https://stackoverflow.com/questions/62853263/pygame-sound-when-started-from-terminal-not-from-nodered
