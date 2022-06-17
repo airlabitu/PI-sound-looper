b16db9c11f3a06bec06e304a3d0e3d90bf7d54e3
Halfdan Hauch Jensen <halfdanhauchjensen@gmail.com>
2022-06-17 13:43:21 +0200

    Update sound_looper.py

--- sound_looper.py
+++ sound_looper.py
@@ -1,14 +1,31 @@
+# this script is an eternal soundfile looper. It plays all .wav and .mp3 files in a folder, and keeps looping the list
+# code by Halfdan Hauch Jensen @AIR LAB ITU, halj@itu.dk
+
 import pygame
+import glob
 import time
 
+pygame.mixer.init()
 time.sleep(10) # wait for the UDB drive to mount
 
-pygame.mixer.init()
-#pygame.mixer.music.load("sound.wav")
-pygame.mixer.music.load("/media/air/AIR_DISK1/sound.wav")
-pygame.mixer.music.play(-1) # note -1 for playing in loops
-while pygame.mixer.music.get_busy(): 
-    pygame.time.Clock().tick(10)
-    
+playlist = list()
+types = ('/media/air/AIR_DISK/*.wav', '/media/air/AIR_DISK/*.mp3')
 
-# https://stackoverflow.com/questions/62853263/pygame-sound-when-started-from-terminal-not-from-nodered
+for files in types: # compiles the directory file list
+    playlist.extend(glob.glob(files))
+
+playlist = sorted(playlist, key = lambda s: s.casefold()) # sorting the list in non case sensitive manner
+
+for i in range(len(playlist)): # prints the list in nice formatted way
+    print(playlist[i])
+
+index = 0
+running = True
+
+while running: # eternal loop
+    if not pygame.mixer.music.get_busy(): # sound stopped, time for next song
+        pygame.mixer.music.load ( playlist[index] )
+        pygame.mixer.music.play()
+        index = index +1
+        if index > len(playlist)-1:
+            index = 0
